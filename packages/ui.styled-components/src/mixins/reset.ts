3f3267c6e20fa4c555934191708cfc7d112a3708
Sergei Gannochenko <gannochenko.sv@gmail.com>
2021-02-15 21:29:54 +0100

    fix: css reset fix [] (#19)

--- packages/ui.styled-components/src/mixins/reset.ts
+++ packages/ui.styled-components/src/mixins/reset.ts
@@ -3,6 +3,7 @@ import { css } from 'styled-components';
 export const reset = () => css`
     box-sizing: border-box;
     margin: 0;
+    padding: 0;
     text-align: left;
     white-space: initial;
 `;
