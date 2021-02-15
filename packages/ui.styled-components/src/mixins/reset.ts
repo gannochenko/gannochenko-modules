2d4c5d01f233d1b9f940fe06617cc298c9e4cf34
Sergei <gannochenko.sv@gmail.com>
2021-02-15 21:23:03 +0100

    fix

--- packages/ui.styled-components/src/mixins/reset.ts
+++ packages/ui.styled-components/src/mixins/reset.ts
@@ -3,6 +3,6 @@ import { css } from 'styled-components';
 export const reset = () => css`
     box-sizing: border-box;
     margin: 0;
-    text-align: right;
+    text-align: left;
     white-space: initial;
 `;
