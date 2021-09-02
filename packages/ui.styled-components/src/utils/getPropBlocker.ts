771a2ee1b2488b265f9592e6cd1eba3d41e3c9d2
Sergei Gannochenko <gannochenko.sv@gmail.com>
2021-09-02 17:58:58 +0200

    fix: propsBlocker fix (#21)

--- packages/ui.styled-components/src/utils/getPropBlocker.ts
+++ packages/ui.styled-components/src/utils/getPropBlocker.ts
@@ -1,6 +1,6 @@
 import { ObjectLiteralType } from '../type';
 
 export const getPropBlocker = (customProps?: ObjectLiteralType<boolean>) => ({
-    shouldForwardProp: (prop: string) =>
+    shouldForwardProp: (prop: string | number) =>
         customProps ? customProps && !(prop in customProps) : true,
 });
