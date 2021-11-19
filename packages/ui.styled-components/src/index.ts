bc3cbdae6ac405e312f957ce47207646314c6a73
Sergei <gannochenko.sv@gmail.com>
2021-11-20 00:47:57 +0100

    wip

--- packages/ui.styled-components/src/index.ts
+++ packages/ui.styled-components/src/index.ts
@@ -2,4 +2,10 @@ export * from './mixins';
 export * from './props';
 export * from './utils';
 
-export type { MUIThemeType, StylePropsType, ScalarType, ObjectLiteralType, BreakpointNameType } from './type';
+export type {
+    MUIThemeType,
+    StylePropsType,
+    ScalarType,
+    ObjectLiteralType,
+    BreakpointNameType,
+} from './type';
